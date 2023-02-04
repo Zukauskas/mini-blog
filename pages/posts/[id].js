eda74538838f81cfc386e2a99d26507f8e8e6451
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-04 17:01:27 +0200

    Small fix

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,8 @@
-import Layout from '../components/layout';
-import Date from '../components/date';
-import utilStyles from '../styles/utils.module.css';
+import Layout from '../../components/layout';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 import Head from 'next/head';
-import { getAllPostIds, getPostData } from '../lib/posts.js';
+import { getAllPostIds, getPostData } from '../../lib/posts.js';
 
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id);
