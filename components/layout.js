0c42aa0ad77d3f8702f27d34c49f38a033b50149
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-05 16:17:58 +0200

    Some cleanup

--- components/layout.js
+++ components/layout.js
@@ -12,24 +12,17 @@ export default function Layout({ children, home }) {
         <div className={styles.container}>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
-                <meta name="description" content="Learn how to build a personal website using Next.js" />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
             </Head>
             <header className={styles.header}>
                 {home ? (
                     <>
-                        <Image priority src="/images/monitor.jpeg" className={utilStyles.borderCircle} height={400} width={400} alt="monitor" />
+                        <Image priority src="/images/monitor.jpeg" height={400} width={400} alt="monitor" />
                         <h1 className={utilStyles.heading2Xl}>{name}</h1>
                     </>
                 ) : (
                     <>
                         <Link href="/">
-                            <Image priority src="/images/monitor.jpeg" className={utilStyles.borderCircle} height={200} width={200} alt="monitor" />
+                            <Image priority src="/images/monitor.jpeg" height={200} width={200} alt="monitor" />
                         </Link>
                         <h2 className={utilStyles.headingLg}>
                             <Link href="/" className={utilStyles.colorInherit}>
